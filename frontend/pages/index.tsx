9a3995b50323fe98950b8c302c797636486f4f8c
Chloe Court <crc.courtois@gmail.com>
2022-11-21 17:21:15 +0100

    added navbar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Navbar } from "../components/nav";
+import { Navbar } from "../components/Navbar";
+import "../i18n";
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
+  const { t } = useTranslation();
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +16,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Navbar />
-        <h1 className="text-3xl font-bold underline">BAD HABITS</h1>
+        <h1 className="text-3xl font-bold underline">{t("badhabits")}</h1>
       </main>
 
       <footer className={styles.footer}>
