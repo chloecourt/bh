035297933669be591a37498427553b067bc733f6
Chloe Court <crc.courtois@gmail.com>
2022-11-25 00:22:37 +0100

    got fetch for landing page image to work

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,48 @@
-// const getHomeScreen = async () => {
-//   const url =
-//     "http://localhost:1337/uploads/arizona_mountainscape_152a1bfac6.jpg";
-//   const options = {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   };
-//   const response = await fetch(url, options);
-//   if (!response.ok) {
-//     console.error(response.statusText);
-//     throw new Error("An error occured please try again");
-//   }
-//   const data = await response.json();
-//   return data;
-// };
-
-export default async () => {
-  // const data = await getHomeScreen();
-  const data = "meow";
-  console.log("this is homescreen fetch", data);
-  return <div>Hi there!</div>;
+const getHomeScreen = async () => {
+  const url = "http://localhost:1337/api/homescreen";
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      console.error(response.statusText);
+      throw new Error("An error occured please try again");
+    }
+    const data = await response.blob();
+    const landingPageImageUrl = URL.createObjectURL(data);
+    return landingPageImageUrl;
+  } catch (e) {
+    console.error("this is the error", e);
+  }
 };
+
+// export function getStrapiMedia(media: any) {
+//   const { url } = media.data.attributes;
+//   // const imageUrl = url.startsWith("/") ? getStrapiURL(url) : url;
+//   return imageUrl;
+// }
+
+const Page = async () => {
+  const data = await getHomeScreen();
+  console.log("does this show?", data);
+
+  return (
+    <div>
+      <span>
+        <h1>hello there!</h1>
+      </span>
+      <img
+        className=""
+        src={
+          "http://localhost:1337/uploads/arizona_mountainscape_152a1bfac6.jpg"
+        }
+        alt="arizona"
+      />
+    </div>
+  );
+};
+
+export default Page;
