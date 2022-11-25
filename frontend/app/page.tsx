278356ace871df38a38fb613d534ad2aac694bbe
Chloe Court <crc.courtois@gmail.com>
2022-11-25 21:23:56 +0100

    added helper functions to parse responses from stapi

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,46 +1,15 @@
-const getHomeScreen = async () => {
-  const url = "http://localhost:1337/api/homescreen";
-  const options = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  try {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      console.error(response.statusText);
-      throw new Error("An error occured please try again");
-    }
-    const data = await response.blob();
-    const landingPageImageUrl = URL.createObjectURL(data);
-    return landingPageImageUrl;
-  } catch (e) {
-    console.error("this is the error", e);
-  }
-};
-
-// export function getStrapiMedia(media: any) {
-//   const { url } = media.data.attributes;
-//   // const imageUrl = url.startsWith("/") ? getStrapiURL(url) : url;
-//   return imageUrl;
-// }
+import { fetchAPI, getStrapiURL } from "../lib/api";
 
 const Page = async () => {
-  const data = await getHomeScreen();
-  console.log("does this show?", data);
+  const { title, description, imageUrl } = await fetchAPI("landing-page");
 
   return (
     <div>
       <span>
-        <h1>hello there!</h1>
+        <h1>{title}</h1>
+        {description && <h4>{description}</h4>}
       </span>
-      <img
-        className=""
-        src={
-          "http://localhost:1337/uploads/arizona_mountainscape_152a1bfac6.jpg"
-        }
-        alt="arizona"
-      />
+      <img className="" src={imageUrl} alt="arizona" />
     </div>
   );
 };
