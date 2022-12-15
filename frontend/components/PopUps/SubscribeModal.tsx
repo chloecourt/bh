8d9f0ed3a9932f5308b34bb50e567a2155a42f9e
Chloe Court <crc.courtois@gmail.com>
2022-12-16 00:14:52 +0100

    improved styling for subscribe modal

--- frontend/components/PopUps/SubscribeModal.tsx
+++ frontend/components/PopUps/SubscribeModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import FormInput from "../sub-components/FormInput";
 
 const SubscribeModal = () => {
   const handleSubmit = () => {
@@ -6,13 +6,25 @@ const SubscribeModal = () => {
   };
 
   return (
-    <div>
-      <h1>Stay up to Date</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="">Stay up to Date</h1>
       <form onSubmit={handleSubmit}>
-        <label>name</label>
-        <input type="input" />
-        <label>email</label>
-        <input type="input" />
+        <FormInput
+          label={""}
+          inputAttributes={{
+            placeholder: "Name",
+            type: "text",
+            required: false,
+          }}
+        />
+        <FormInput
+          label={""}
+          inputAttributes={{
+            placeholder: "Email",
+            type: "email",
+            required: false,
+          }}
+        />
       </form>
     </div>
   );
